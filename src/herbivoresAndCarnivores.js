a0ed2ecb83edd3827dd5c1cd863e5227e0e9f3ce
Alyona Verbanova <alyona.verbanova22@gmail.com>
2024-11-13 18:47:01 +0100

    create classes

--- src/herbivoresAndCarnivores.js
+++ src/herbivoresAndCarnivores.js
@@ -1,15 +1,46 @@
 'use strict';
 
 class Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    this.name = name;
+    this.health = health;
+    Animal.alive.push(this);
+  }
+  static alive = [];
+
+  static animalHealth(animal) {
+    if (animal.health <= 0) {
+      Animal.alive = Animal.alive.filter((a) => a !== animal);
+    }
+  }
 }
 
 class Herbivore extends Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    super(name, health);
+    this.hidden = false;
+  }
+  hide() {
+    this.hidden = true;
+  }
 }
 
 class Carnivore extends Animal {
-  // write your code here
+  constructor(name, health = 100) {
+    super(name, health);
+  }
+
+  bite(herb) {
+    if (herb instanceof Herbivore && !herb.hidden) {
+      herb.health -= 50;
+
+      Animal.animalHealth(herb);
+    }
+
+    if (herb instanceof Carnivore) {
+      return herb.health;
+    }
+  }
 }
 
 module.exports = {
